768878174bc47a868dc851666f062099582ec3ae
Antonia <anrybaczyk@gmail.com>
2026-06-22 15:47:04 +0200

    first commit

---
+++ src/main/java/de/htw_berlin/fb4/ossd/antonia/HelloSentence.java
@@ -0,0 +1,12 @@
+package de.htw_berlin.fb4.ossd.antonia;
+
+import de.htw_berlin.fb4.ossd.prose.Sentence;
+
+public class HelloSentence implements Sentence {
+
+    public String get() {
+        return "Hallo";
+    }
+
+
+}
